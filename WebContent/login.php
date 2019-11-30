359152d426e7de90a94b618687c87314cc2f236c
NiklasHerrmann21 <niklasherrmann96@aol.de>
2019-11-30 12:52:20 +0100

    Login

--- WebContent/login.php
+++ WebContent/login.php
@@ -1,31 +1,29 @@
 <?php
-$connect = mysqli_connect("localhost", "root", "", "povalueuser"); 
+$connect = mysqli_connect("localhost", "root", "", "povalueuser");
    session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $_email = mysqli_real_escape_string($connect,$_POST['email']);
-      $_password = mysqli_real_escape_string($connect,$_POST['passwort']); 
-      
-	   $_sql = "SELECT * FROM user WHERE
-                    email='$_email' AND
-                    passwort='$_password'
-                LIMIT 1";
 
-      
-      $result = mysqli_query($_sql, $connect);
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+      $_email = mysqli_real_escape_string($connect,$_POST['email']);
+      $_password = mysqli_real_escape_string($connect,$_POST['passwort']);
+
+	   $_sql = "SELECT * from user Where email='".$_email."' AND passwort='".$_password."';";
+
+       $result = mysqli_query($connect, $_sql);
+
       $count = mysqli_num_rows($result);
+      echo $count;
       
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
       if($count == 1) {
-         session_register("myemail");
-         $_SESSION['login_user'] = $myemail;
-         
-         header("location: main.html");
+          
+      
+
+          header("location: main.html");
       }else {
-         $error = "Your Login Name or Password is invalid";
+          // header("location: index.html");
+          $error = 'Your Login Name or Password is invalid';
+          
       }
    }
 ?>
