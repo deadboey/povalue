eee06880dd2addf21d5879f2283a4da9b38ec3db
Simzu99 <50910561+Simzu99@users.noreply.github.com>
2019-12-04 00:37:06 +0100

    Hamburger

--- WebContent/main.php
+++ WebContent/main.php
@@ -165,5 +165,8 @@ data-i18n_after_dislike="Sau bled" data-i18n_like_tooltip="Klick auf mich!"
 data-i18n_dislike_tooltip="Klick auf mich!" data-i18n_unlike_tooltip="Andere Meinung?" 
 data-i18n_undislike_tooltip="Andere Meinung?"></span>
 
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </body>
 </html>
