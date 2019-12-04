8dad002e4f830be30388d24354272d231fb07447
Simzu99 <50910561+Simzu99@users.noreply.github.com>
2019-12-04 11:52:53 +0100

    Karusell neuer Header, ohne Farbe weis

--- WebContent/main.php
+++ WebContent/main.php
@@ -12,12 +12,16 @@ include("geheim.php");
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="main.css">
   <link rel="canonical" href="https://getbootstrap.com/docs/4.4/">
-	<title>PotValue - Get your Post evaluated</title> 
+	<title>PotValue - Get your Post evaluated</title>
+
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/gallery/style.css">
 </head>
 
   <body>
     <nav class="navbar navbar-dark bg-dark">
-  <a class="navbar-brand" href="main.html">PoValue <?php echo "heißt dich Willkommen ", $_SESSION['user'], "!" ; ?>  </a>
+  <a class="navbar-brand" href="main.php">PoValue <?php echo "heißt dich Willkommen ", $_SESSION['user'], "!" ; ?>  </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -40,119 +44,124 @@ include("geheim.php");
   </div>
 </nav>
 
-<div class="jumbotron">
-  <div class="container">
-    <h1 class="headline">Hello, DomB!</h1>
-    <p>Wir hoffen dein Dünnschiss hat sich beruhigt und du musst jetzt nicht nur noch kacken, sondern kannst endlich mit uns coden. In Liebe, dein Team :)</p>
-  </div>
-</div>
-
-<!--- Altes Karusell
-<div>
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-            <img src="img/DSC_0306.JPG" class="d-block w-100" alt="0">
-      </div>
-      <div class="carousel-item">
-         <img src="img/metin-1.jpeg" class="d-block w-100" alt="1">
-      </div>
-      <div class="carousel-item">
-          <img src="img/metin-2.jpeg" class="d-block w-100" alt="2">
-      </div>
-      <div class="carousel-item">
-          <img src="img/musicianjoke.png" class="d-block w-100" alt="3">
-    </div>
-      <div class="carousel-item">
-           <img src="img/00A292A1-C663-4B4A-A67B-B6133412838F.jpeg" class="d-block w-100" alt="4">
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Zurück</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Vorwärts</span>
-    </a>
-  </div>
-</div>
---->
-
-<!---Gallerie--->
-<!---Reihe 1--->
-    <div>
-      <div class="column">
-        <img src="img/DSC_0306.JPG" alt="0">
-        <span class="icon-focus"></span>
-      </div>
-      <div class="column">
-      <img src="img/DSC_0490.JPG" alt="0">
-        <span class="icon-focus"></span>
-      </div>
-      <div class="column">
-      <img src="img/DSC_0500.JPG" alt="0">
-        <span class="icon-focus"></span>
-      </div>
-      <div class="column">
-      <img src="img/DSC_0501.JPG" alt="0">
-        <span class="icon-focus"></span>
-      </div>
-    </div>
-    <!---Reihe 2--->
-    <div>
-      <div class="column">
-      <img src="img/DSC_0731.JPG" alt="0">
-        <span class="icon-focus"></span>
-      </div>
-      <div class="column">
-      <img src="img/DSC_0733.JPG" alt="0">
-        <span class="icon-focus"></span>
-      </div>
-      <div class="column">
-      <img src="img/DSC_0754.JPG" alt="0">
-        <span class="icon-focus"></span>
-      </div>
-      <div class="column">
-      <img src="img/DSC_0755.JPG" alt="0">
-        <span class="icon-focus"></span>
-      </div>
-    </div>
-
-<!--- Slider???
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="false" data-keyboard="true">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
-    </ol>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Zurück</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Vorwärts</span>
-      </a>
-      <a class="close" role="button" href="#" data-dismiss="modal">
-        <span class="sr-only">Close</span>
-      </a>
+<section>
+  <div>
+    <div class="header" style="background-image: url(img/background.jpg); padding: 5px; width: 100%; height: 10%;" >
+      <h2 class="ue1">WILLKOMMEN BEI POVALUE</h2>
+      <h3 class="ue2">Werden Sie Teil unserer Community und bewerten Bilder anderer Mitglieder!</h3>
     </div>
   </div>
-</div>
---->
+</section>
+
+<section class="mbr-gallery mbr-slider-carousel cid-rJFQcPPPSH" id="gallery3-7">
+  <div>
+      <div>
+        <div class="mbr-gallery-row">
+          <div class="mbr-gallery-layout-default">
+            <div>
+              <div>
+                <div class="mbr-gallery-item mbr-gallery-item--p0" data-video-url="false">
+                  <div href="#lb-gallery3-7" data-slide-to="0" data-toggle="modal">
+                    <img src="img/DSC_0306.JPG" alt="" title="">
+                    <span class="icon-focus"></span>
+                  </div>
+                </div>
+                <div class="mbr-gallery-item mbr-gallery-item--p0" data-video-url="false">
+                  <div href="#lb-gallery3-7" data-slide-to="1" data-toggle="modal">
+                    <img src="img/DSC_0490.JPG" alt="" title="">
+                    <span class="icon-focus"></span>
+                  </div>
+                </div>
+                <div class="mbr-gallery-item mbr-gallery-item--p0" data-video-url="false">
+                  <div href="#lb-gallery3-7" data-slide-to="2" data-toggle="modal">
+                   <img src="img/DSC_0500.JPG" alt="" title="">
+                    <span class="icon-focus"></span>
+                  </div>
+                </div>
+                <div class="mbr-gallery-item mbr-gallery-item--p0" data-video-url="false">
+                  <div href="#lb-gallery3-7" data-slide-to="2" data-toggle="modal">
+                   <img src="img/DSC_0501.JPG" alt="" title="">
+                    <span class="icon-focus"></span>
+                  </div>
+                </div>
+                <div class="mbr-gallery-item mbr-gallery-item--p0" data-video-url="false">
+                  <div href="#lb-gallery3-7" data-slide-to="4" data-toggle="modal">
+                    <img src="img/DSC_0731.JPG" alt="" title="">
+                    <span class="icon-focus"></span>
+                  </div>
+                </div>
+                <div class="mbr-gallery-item mbr-gallery-item--p0" data-video-url="false">
+                  <div href="#lb-gallery3-7" data-slide-to="5" data-toggle="modal">
+                  <img src="img/DSC_0733.JPG" alt="" title="">
+                  <span class="icon-focus"></span>
+                  </div>
+                </div>
+                <div class="mbr-gallery-item mbr-gallery-item--p0" data-video-url="false">
+                  <div href="#lb-gallery3-7" data-slide-to="6" data-toggle="modal">
+                    <img src="img/DSC_0754.JPG" alt="" title="">
+                    <span class="icon-focus"></span>
+                  </div>
+                </div>
+                <div class="mbr-gallery-item mbr-gallery-item--p0" data-video-url="false">
+                  <div href="#lb-gallery3-7" data-slide-to="7" data-toggle="modal">
+                    <img src="img/DSC_0755.JPG" alt="" title="">
+                    <span class="icon-focus"></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="clearfix">
+            </div>
+          </div>
+        </div><!-- Lightbox -->
+        <div data-app-prevent-settings="" class="mbr-slider modal fade carousel slide" tabindex="-1" data-keyboard="true" data-interval="false" id="lb-gallery3-7">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-body">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="img/DSC_0306.JPG" alt="" title="">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="img/DSC_0490.JPG" alt="" title="">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="img/DSC_0500.JPG" alt="" title="">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="img/DSC_0501.JPG" alt="" title="">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="img/DSC_0731.JPG" alt="" title="">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="img/DSC_0733.JPG" alt="" title="">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="img/DSC_0754.JPG" alt="" title="">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="img/DSC_0755.JPG" alt="" title="">
+                  </div>
+                </div>
+                <a class="carousel-control carousel-control-prev" role="button" data-slide="prev" href="#lb-gallery3-7">
+                  <span class="mbri-left mbr-iconfont" aria-hidden="true"></span>
+                  <span class="sr-only">Rückwärts</span>
+                </a>
+                <a class="carousel-control carousel-control-next" role="button" data-slide="next" href="#lb-gallery3-7">
+                  <span class="mbri-right mbr-iconfont" aria-hidden="true"></span>
+                  <span class="sr-only">Vorwärts</span>
+                </a>
+                <a class="close" href="#" role="button" data-dismiss="modal">
+                  <span class="sr-only">Schließen</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</section>
 
 <span class="likebtn-wrapper" data-theme="custom" data-btn_size="30" data-f_size="25" 
 data-icon_size="25" data-icon_l="thmb4-u" data-icon_d="thmb4-d" data-icon_l_c="#000000" 
@@ -167,6 +176,15 @@ data-i18n_undislike_tooltip="Andere Meinung?"></span>
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+ 
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/masonry/masonry.pkgd.min.js"></script>
+  <script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/gallery/player.min.js"></script>
+  <script src="assets/gallery/script.js"></script>
+  <script src="assets/slidervideo/script.js"></script>
 
 </body>
 </html>
